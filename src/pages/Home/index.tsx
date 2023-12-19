177bac8135f996a94bfb0b64d8c72855b43d44f5
richardenrico <richardenrico02@gmail.com>
2023-12-19 17:13:54 +0800

    fix: change SEO page title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,7 +24,7 @@ const Home = () => {
 
     return (
         <>
-            <Seo title={'Hello World'} />
+            <Seo title={'FAQ Accordion | Front-end Mentor Challenge'} />
             <Container
                 fluid
                 p={0}
