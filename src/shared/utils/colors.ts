1de08d5774b4041b5d227136524b816218cf4b0a
richardenrico <richardenrico02@gmail.com>
2023-12-19 16:35:16 +0800

    feat: add colors

--- src/shared/utils/colors.ts
+++ src/shared/utils/colors.ts
@@ -1,8 +1,9 @@
-export type Colors = 'white' | 'light-pink' | 'grayish-purple' | 'dark-purple';
+export type Colors = 'white' | 'light-pink' | 'grayish-purple' | 'dark-purple' | 'purple';
 
 export const COLORS: Record<Colors, string> = {
     white: '#FFFFFF',
     'light-pink': '#f9f0ff',
     'grayish-purple': '#8c6991',
     'dark-purple': '#2f1533',
+    purple: '#AD28EB'
 };
