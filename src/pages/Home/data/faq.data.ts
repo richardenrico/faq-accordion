a55c495c2b3e516376b428c978dcbd4972f3ce06
richardenrico <richardenrico02@gmail.com>
2023-12-19 16:35:25 +0800

    feat: add faq data

---
+++ src/pages/Home/data/faq.data.ts
@@ -0,0 +1,17 @@
+export const faqData = [
+    {
+        value: 'Apples',
+        description:
+            'Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads.',
+    },
+    {
+        value: 'Bananas',
+        description:
+            'Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking.',
+    },
+    {
+        value: 'Broccoli',
+        description:
+            'Nutrient-packed green vegetable. Broccoli is packed with vitamins, minerals, and fiber. It has a distinct flavor and can be enjoyed steamed, roasted, or added to stir-fries.',
+    },
+];
